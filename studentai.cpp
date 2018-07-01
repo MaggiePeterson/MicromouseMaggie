a8537f6a6adb3cf50d556c87a65b2bdb410275c3
MaggiePeterson <32180050+MaggiePeterson@users.noreply.github.com>
2018-06-30 20:35:40 -0700

    made class square, logs walls

--- studentai.cpp
+++ studentai.cpp
@@ -9,6 +9,45 @@ using namespace std;
      2
 */
 
+class square { //SHORTEST PATH: make square class in array, detects where walls are
+
+    bool isWallNorth;
+    bool isWallEast;
+    bool isWallSouth;
+    bool isWallWest;
+
+public:
+    void setWallNorth(bool wallN);
+    void setWallEast(bool wallE);
+    void setWallSouth(bool wallS);
+    void setWallWest(bool wallW);
+
+};
+
+void square::setWallNorth(bool wallN) //wall is north if value gets is true/ false
+{
+    this->isWallNorth = wallN;
+}
+
+void square::setWallEast(bool wallE)
+{
+    this->isWallEast = wallE;
+}
+
+void square::setWallSouth(bool wallS)
+{
+    this->isWallSouth = wallS;
+}
+
+void square::setWallWest(bool wallW)
+{
+    this->isWallWest = wallW;
+}
+
+square mapSq[20][20];
+
+//---------------------------
+
 void myTurnLeft(int *dir) //changes direction of mouse everytime turns left
 {
    if (*dir ==0)
@@ -27,39 +66,39 @@ void myTurnRight(int *dir) //sets direction of mouse everytime turns right
         *dir+=1;
 }
 
-void myMoveForward(int *x, int *y, int *dir, int (&mazeMap)[41][41]) //changes x or y coodinates when moving forward
+void myMoveForward(int *x, int *y, int *dir, int (&mazeMap)[20][20]) //changes x or y coodinates when moving forward
 {
      mazeMap[*x][*y]+=1; //increments value in that position, counts how many times been to coodinate
 
-    if (*dir == 0) //+2 because space in middle is space for wall
-        *y-=2;
+    if (*dir == 0)
+        *y-=1;
 
     else if (*dir==1)     
-        *x+=2;
+        *x+=1;
 
     else if (*dir==2)    
-        *y+=2;
+        *y+=1;
 
     else // dir ==3     
-        *x-=2;
+        *x-=1;
 
 }
 
-int numberOfTimesLeft(int dir, int x, int y, int (&mazeMap)[41][41] )
+int numberOfTimesLeft(int dir, int x, int y, int (&mazeMap)[20][20] )
 {
     myTurnLeft(&dir);
     myMoveForward(&x, &y, &dir, mazeMap);
     return mazeMap[x][y];
 }
 
-int numberOfTimesRight(int dir, int x, int y, int (&mazeMap)[41][41] )
+int numberOfTimesRight(int dir, int x, int y, int (&mazeMap)[20][20] )
 {
     myTurnRight(&dir);
     myMoveForward(&x, &y, &dir, mazeMap);
     return mazeMap[x][y];
 }
 
-int numberOfTimesForward(int dir, int x, int y, int (&mazeMap)[41][41] )
+int numberOfTimesForward(int dir, int x, int y, int (&mazeMap)[20][20] )
 {
     myMoveForward(&x, &y, &dir, mazeMap);
     return mazeMap[x][y];
@@ -69,12 +108,13 @@ void microMouseServer::studentAI()
 {
    static int counterR; //for ending, counts how many times mouse turnsRight and moveForward
    static int counterL;
-   static int x=1;
-   static int y=39;
+   static int x=0;
+   static int y=19; //shows in bottom left corner of array
    static int dir = 0;
-   static int mazeMap[41][41]= {0}; //41 spaces, walls have space in array
+   static int mazeMap[20][20]= {0}; //20 spaces, walls have space in array
 
-   //find finish (first time) ----------------------------------
+    //use enum, find state to get exploring vs. shortest path
+   //find finish (first time) ------------------------------------
 
    if (!isWallLeft())
     {
@@ -124,68 +164,57 @@ void microMouseServer::studentAI()
            counterR = 0;
            counterL= 0;
        }
-
     }
 
    moveForward();
    myMoveForward(&x, &y, &dir, mazeMap);
 
-   //find where walls are
+   //find where walls are------------------------
 
    switch (dir){
-       case 0: //mouse is forward
-       {
-            if(isWallForward())
-                mazeMap[x][y-1] = 99;
-            if(isWallRight())
-                mazeMap[x+1][y] = 99;
-            if(isWallLeft())
-                mazeMap[x-1][y] = 99;
-       }
-       case 1: //mouse is right
-       {
-            if(isWallForward())
-                mazeMap[x+1][y] = 99;
-            if(isWallRight())
-                mazeMap[x][y+1] = 99;
-            if(isWallLeft())
-                mazeMap[x][y-1] = 99;
-       }
-       case 2: //mouse is down
-       {
-            if(isWallForward())
-                mazeMap[x][y+1] = 99;
-            if(isWallRight())
-                mazeMap[x-1][y] = 99;
-            if(isWallLeft())
-                mazeMap[x+1][y] = 99;
-       }
-       case 3: //mouse is left
-       {
-            if(isWallForward())
-                mazeMap[x-1][y] = 99;
-            if(isWallRight() )
-                mazeMap[x][y-1] = 99;
-            if(isWallLeft() )
-                mazeMap[x][y+1] = 99;
-       }
+    case 0: //forward
+      {
+       mapSq[x][y].setWallNorth(isWallForward());
+       mapSq[x][y].setWallEast(isWallRight());
+       mapSq[x][y].setWallWest(isWallLeft());
+       break;
+      }
+   case 1: //right
+     {
+      mapSq[x][y].setWallEast(isWallForward()); //forward if mouse is facing right, square has wall East
+      mapSq[x][y].setWallSouth(isWallRight());
+      mapSq[x][y].setWallNorth(isWallLeft());
+      break;
+     }
+   case 2: //down
+     {
+      mapSq[x][y].setWallSouth(isWallForward()); //is facing down, wall is forward, wall is objectively south
+      mapSq[x][y].setWallWest(isWallRight());
+      mapSq[x][y].setWallEast(isWallLeft());
+      break;
+     }
+   case 3: //left
+     {
+      mapSq[x][y].setWallWest(isWallForward());
+      mapSq[x][y].setWallNorth(isWallRight());
+      mapSq[x][y].setWallSouth(isWallLeft());
+      break;
+     }
    }
 
-
-
-
-    if (counterR == 3 || counterL == 3)
+   //finish-----------------------------------
+    if (counterR > 2 || counterL > 2 )
        {
         foundFinish();
        }
 
-
     cout<<endl;
-        for(int i=0; i<41; i++){
-            for(int j=0; j<41; j++){
+        for(int i=0; i<20; i++){
+            for(int j=0; j<20; j++){
                 cout<<mazeMap[j][i]<<" ";
             }
             cout<<endl;
        }
         cout<<endl;
 }
+
