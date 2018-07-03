4beb189faca7bdf6e84e64d4379904bedf1ef5f9
MaggiePeterson <32180050+MaggiePeterson@users.noreply.github.com>
2018-07-02 22:16:22 -0700

    added flag bool

--- studentai.cpp
+++ studentai.cpp
@@ -13,9 +13,10 @@ using namespace std;
 
 struct node {
 
-    int timesVisited,xN,yN;
+    int timesVisited;
+    int xN =0,yN=19;
     bool finished = false;
-    bool visitedNode = false;
+    bool flag = false;      //visited Node
 
     node* previousNode = NULL;
     node* north = NULL;
@@ -43,24 +44,21 @@ void myTurnRight(int *dir) //sets direction of mouse everytime turns right
         *dir+=1;
 }
 
-void myMoveForward(int *x, int *y, int *dir, node (mazeMap)[20][20]) //changes x or y coodinates when moving forward
+void myMoveForward(int *x, int *y, int *dir, node (&mazeMap)[20][20]) //changes x or y coodinates when moving forward
 {
      mazeMap[*x][*y].timesVisited+=1; //increments value in that position, counts how many times been to coodinate
 
     if (*dir == 0)
 
         *y-=1;
-    else if (*dir==1)
 
+    else if (*dir==1)
         *x+=1;
 
-
     else if (*dir==2)
-
         *y+=1;
 
-    else // dir ==3
-
+    else // 3
         *x-=1;
 }
 
@@ -84,8 +82,7 @@ int numberOfTimesForward(int dir, int x, int y, node (&mazeMap)[20][20] )
     return mazeMap[x][y].timesVisited;
 }
 
-
-void findShortestPath(int x, int y, node (&mazeMap)[20][20] )
+void findShortestPath(int x, int y, node (&mazeMap)[20][20] ) //BFR algorithm
 {
      QQueue <node*> q;              //if visisted node once dont go back, boolean
      q.enqueue(& (mazeMap[0][19])); //adds starting position to queue
@@ -94,61 +91,60 @@ void findShortestPath(int x, int y, node (&mazeMap)[20][20] )
       {
         node* currNode = q.dequeue(); //currNode points to removed value
 
-        if (currNode->north != NULL && !currNode->visitedNode) //finds adjacent nodes, doesnt count node if already counted
+        if (currNode->north != NULL && !currNode->north->flag) //finds adjacent nodes, doesnt count node if already counted
          {
             currNode->north->previousNode = currNode;       //currNode is previous node of node North
             q.enqueue(currNode->north);                     //adds north node to queue
-            currNode->visitedNode = true;
+            currNode->north->flag = true;
          }
-         if(currNode->east != NULL && !currNode->visitedNode) //opening right && if node has not been checked/ visited
+         if(currNode->east != NULL && !currNode->east->flag) //opening right && if node has not been checked/ visited
         {
             currNode->east->previousNode = currNode;
             q.enqueue(currNode->east);
-            currNode->visitedNode = true; //node has been visited/ checked
+            currNode->east->flag = true; //node has been visited/ checked
         }
-        if(currNode->west != NULL && !currNode->visitedNode) //opening left...
+        if(currNode->west != NULL && !currNode->west->flag) //opening left...
         {
             currNode->west->previousNode = currNode;
             q.enqueue(currNode->west);
-            currNode->visitedNode = true;
+            currNode->west->flag = true;
         }
-        if(currNode->south != NULL && !currNode->visitedNode) //opening down...
+        if(currNode->south != NULL && !currNode->south->flag) //opening down...
         {
             currNode->south->previousNode = currNode;
             q.enqueue(currNode->south);
-            currNode->visitedNode = true;
+            currNode->south->flag = true;
         }
       }
 
      node* pathNode = &(mazeMap[x][y]); //sets pathNode pointer to position of finish
 
-     while(pathNode->xN != 0 && pathNode->yN != 19) //finds shortest path, marks spots as -99
+     while(pathNode->xN != 0 && pathNode->yN != 19) //until x,y of pathnode are 0,19 (at beginnning), backtrack from finish to start
      {
-            pathNode->timesVisited = -99;
-            pathNode = pathNode->previousNode; //pathnode points to previous node, goes back in path
+            pathNode->timesVisited = -99; //mark path from finish to start with -99 (least visited)
+            pathNode = pathNode->previousNode; //pathnode points to previous node, goes back in path, continues
       }
 
 }
 void microMouseServer::studentAI()
 {
-   static int counterR; //for ending, counts how many times mouse turnsRight and moveForward
-   static int counterL;
-   static int x=0;
-   static int y=19; //shows in bottom left corner of array
+   static int counterR, counterL; //for ending, counts how many times mouse turnsRight and moveForward
+   static int x=0, y=19;    //shows in bottom left corner of array
    static int dir = 0;
-   static node mazeMap[20][20];
+   static node mazeMap[20][20]; //map of nodes
 
+   //creates links between nodes when no wall separates
    switch (dir){
     case 0: //forward
       { if(!isWallForward())
           {
-           mazeMap[x][y].north = & (mazeMap[x][y-1]);
-           mazeMap[x][y-1].south = &(mazeMap[x][y] );
+           mazeMap[x][y].north = & (mazeMap[x][y-1]); //north of node becomes upper node
+           mazeMap[x][y-1].south = &(mazeMap[x][y] ); //south of upper node becomes lower node
            }
        else if (!isWallRight())
        {
-           mazeMap[x][y].east = & (mazeMap[x-1][y]);
-           mazeMap[x-1][y].west = &(mazeMap[x][y] );
+           mazeMap[x][y].east = & (mazeMap[x+1][y]);
+           mazeMap[x+1][y].west = &(mazeMap[x][y] );
         }
        else if(!isWallLeft())
        {
@@ -157,26 +153,26 @@ void microMouseServer::studentAI()
         }
 
        break;
-       }
+     }
     case 1: //right
-       {
+     {
        if(!isWallForward())
        {
           mazeMap[x][y].east = &(mazeMap[x+1][y]);
           mazeMap[x+1][y].west = &(mazeMap[x][y]);
        }
-       else if(!isWallRight())
+       if(!isWallRight())
         {
            mazeMap[x][y].south = &(mazeMap[x][y+1]);
            mazeMap[x][y+1].north = &(mazeMap[x][y]);
         }
-       else if(!isWallLeft())
+       if(!isWallLeft())
        {
            mazeMap[x][y].north = &(mazeMap[x][y-1]);
            mazeMap[x][y-1].south = &(mazeMap[x][y]);
        }
        break;
-        }
+      }
      case 2: //down
      {
        if(!isWallForward())
@@ -184,12 +180,12 @@ void microMouseServer::studentAI()
            mazeMap[x][y].south = &(mazeMap[x][y+1]);
            mazeMap[x][y+1].north = &(mazeMap[x][y]);
        }
-       else if(!isWallRight())
+       if(!isWallRight())
        {
            mazeMap[x][y].west = &(mazeMap[x-1][y]);
            mazeMap[x-1][y].east = &(mazeMap[x][y]);
        }
-       else if(!isWallLeft())
+      if(!isWallLeft())
        {
            mazeMap[x][y].east = &(mazeMap[x+1][y]);
            mazeMap[x+1][y].west = &(mazeMap[x][y]);
@@ -203,18 +199,18 @@ void microMouseServer::studentAI()
             mazeMap[x][y].west = &(mazeMap[x-1][y]);
             mazeMap[x-1][y].east = &(mazeMap[x][y]);
          }
-         else if(!isWallRight())
+        if(!isWallRight())
         {
             mazeMap[x][y].north = &(mazeMap[x][y-1]);
             mazeMap[x][y-1].south = &(mazeMap[x][y]);
          }
-        else if(!isWallLeft())
+        if(!isWallLeft())
         {
             mazeMap[x][y].south = &(mazeMap[x][y+1]);
             mazeMap[x][y+1].north = &(mazeMap[x][y]);
         }
         break;
-        }
+     }
   }
 
    //ISLANDED ALGORITHM ------------------------------------
@@ -274,10 +270,10 @@ void microMouseServer::studentAI()
    moveForward();
    myMoveForward(&x, &y, &dir, mazeMap);
 
-   //finish-----------------------------------
+   //finish
     if (counterR > 2 || counterL > 2 )
        {
-        mazeMap[x][y].finished = true; //
+        mazeMap[x][y].finished = true;
         findShortestPath(x,y,mazeMap); //find shortest path w/ nodes
         foundFinish();
        }
